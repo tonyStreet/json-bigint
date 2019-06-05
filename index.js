c1070fb3b0c01f172097e6079a9b90b6662def68
Anthony Lansangan <anthony.lansangan@lalamove.com>
2019-06-05 17:42:19 +0800

    UPT index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var json_stringify = require('./lib/stringify.js').stringify;
-var json_parse     = require('./lib/parse.js');
-
-module.exports = function(options) {
+var JSONBig = (function(options) {
     return  {
         parse: json_parse(options),
         stringify: json_stringify
     }
-};
-//create the default method members with no options applied for backwards compatibility
-module.exports.parse = json_parse();
-module.exports.stringify = json_stringify;
+});
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+	module.exports = JSONBig;
+} else {
+    window.Validator = JSONBig;
+}
